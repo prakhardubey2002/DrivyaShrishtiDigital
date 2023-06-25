08a0898aa8a8f761e53804b4977d83baca8fab89
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-06-25 17:13:29 +0530

    Front page complted,typewritter effectadded,lottieanimation added site made responsive

---
+++ Js/App.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text-slider", {
+    strings: [
+        // "Customers",
+        "Reach",
+        "Profit",
+        "Growth",
+        "Sales",
+        // "Productivity",
+
+    ],
+    typeSpeed: 90,
+    backSpeed: 60,
+    loop: true
+});
+function myfunction() {
+    var preloader = document.getElementById('loader');
+    preloader.style.display = 'none';
+}
